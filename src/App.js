a83138a42ec21dcdaa2eedb4138d3f32674bb7b2
tejas <upadhyaytejas100@gmail.com>
2023-07-05 11:14:13 +0530

    button color change on onClick

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import ReactPlayer from 'react-player'
 
 function App() {
-  const [like, setlike] = useState(0),
-    [isLike, setIsLike] = useState(false),
-    
-    onLikeButtonClick = () => {
-      setlike(like + (isLike ? -1 : 1));
-      setIsLike (!isLike);
-    }
+  const [like, setLike] = useState(0);
+  const [isLike, setIsLike] = useState(false);
+  const [buttonColor, setButtonColor] = useState('');
+
+  const onLikeButtonClick = () => {
+    setLike(like + (isLike ? -1 : 1));
+    setIsLike(!isLike);
+    setButtonColor(isLike ? '' : 'blue');
+  };
 
   return (
     <div>
@@ -17,7 +19,7 @@ function App() {
     <div className='appContainer'>
     <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
       <p className={"" + (isLike ? "text-primary" : "")}>
-        <i class="fa fa-thumbs-up" style={{fontSize:"30px", padding:"10px"}} onClick={onLikeButtonClick}></i>
+        <i class="fa fa-thumbs-up" style={{fontSize:"30px", padding:"10px", color: buttonColor}} onClick={onLikeButtonClick}></i>
         <br />
         <p className='text-dark'>Like {like}</p>
       </p>
